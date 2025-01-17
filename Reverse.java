d047398260e24da980cab829a93807b649a39723
Ashutosh Yadav <ashutosh08yadav@gmail.com>
2025-01-17 16:12:25 +0530

    Reverse a number

--- Reverse.java
+++ Reverse.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class Reverse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int rev = 0;
+        int n = sc.nextInt();  //500        50  5
+        String rev="";
         while (n != 0) {
-            int d = n % 10;
-            rev = rev * 10 + d;
-            n = n / 10;
+            int d = n % 10;  //500%10==0 d==0   d==0     d==5
+            rev = rev + d;   //  rev=0;     00     005
+            n = n / 10;       //50      5
         }
         System.out.println(rev);
     }
